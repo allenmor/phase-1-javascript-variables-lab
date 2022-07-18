f2f4e841defb4b3c059254c87e5fcba173555952
Allen Mordukhaev <allenmor93@gmail.com>
2022-07-18 13:00:48 -0400

    commit tree 7a16473ff869959abc1f9bda3e5e50067158bc00

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution in this file!
+
+// First test to past make company name Scuber
+
+const companyName = 'Scuber';
+
+let mostProfitableNeighborhood = 'Chelsea';
+
+let companyCeo = 'Susan Smith';
